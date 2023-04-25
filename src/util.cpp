2a2f1307132a893d6ee61ae5c959c9ac51077ba3
paul666chen <572305451@qq.com>
2023-04-25 14:21:50 +0800

    util

---
+++ src/util.cpp
@@ -0,0 +1,45 @@
+#pragma once
+#include "util.h"
+#include<iostream>
+int TestOut()
+{
+
+    int a = 1;
+    return a;
+}
+
+int TestOut(int a)
+{
+
+    int temp = a;
+    return --temp;
+}
+int TestOut(int a, int b){
+
+    return a*b;
+}
+
+
+int TestOut(int & a){
+
+    return a=10;
+}
+
+int TestOut(int & a, int & b)
+{
+
+    return a&b;
+
+}
+std::string TestOutStr(std::string s){
+
+    std::cout<<"Testing string"<<std::endl;
+
+    for (size_t i = 0; i < s.size(); i++)
+    {
+       std:: cout<<s[i]<<std::endl;
+    }
+    
+
+
+}
